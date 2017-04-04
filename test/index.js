8e70c4d39b5effc1d15ffa50fcdd52f576767a35
Tyler Long <tyler4long@gmail.com>
2017-04-04 20:59:17 +0800

    Get rid of jest

--- test/index.js
+++ test/index.js
@@ -1,26 +1,29 @@
-/* eslint-env jest */
+import assert from 'assert'
 import markdownIt from 'markdown-it'
 import markdownItHighlight from '../src/index'
 
 const mdi = markdownIt()
 mdi.use(markdownItHighlight)
 
-test('code block', () => {
-  expect(mdi.render(`
+console.log(mdi.render(`
     print 'hello world'
-`).trim()).toBe(`<pre><code>print 'hello world'
+`))
+assert(mdi.render(`
+    print 'hello world'
+`).trim() === `<pre><code>print 'hello world'
 </code></pre>`)
-})
 
-test('fence without language name', () => {
-  expect(mdi.render(`\`\`\`
+console.log(mdi.render(`\`\`\`
 print 'hello world'
-\`\`\``).trim()).toBe(`<pre><code>print 'hello world'
+\`\`\``))
+assert(mdi.render(`\`\`\`
+print 'hello world'
+\`\`\``).trim() === `<pre><code>print 'hello world'
 </code></pre>`)
-})
 
-test('fence with language name', () => {
-  expect(mdi.render(`\`\`\`python
+console.log(mdi.render(`\`\`\`python
 print 'hello world'
-\`\`\``).trim()).toBe(`<pre><code class="hljs"><span class="hljs-keyword">print</span> <span class="hljs-string">'hello world'</span></code></pre>`)
-})
+\`\`\``))
+assert(mdi.render(`\`\`\`python
+print 'hello world'
+\`\`\``) === `<pre><code class="hljs"><span class="hljs-keyword">print</span> <span class="hljs-string">'hello world'</span></code></pre>`)
